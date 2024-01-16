96f3bfc7d320bd7783d1d0d52addd4f72efca415
duongtranngoc <duongtn0306@gmail.com>
2024-01-17 04:31:30 +0700

    database connection

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,4 +1,26 @@
-const MONGODB_URL =
+const MONGODB_URI =
   "mongodb+srv://duongtn0306:nomalakk1@cluster0.g1ihjwf.mongodb.net/?retryWrites=true&w=majority";
-
 const DATABASE_NAME = "trello";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let trelloDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance)
+    throw new Error("Must connect to Databse first!");
+  return trelloDatabaseInstance;
+};
--- src/server.js
+++ src/server.js
@@ -1,28 +1,32 @@
+/* eslint-disable no-console */
 import express from "express";
-import { mapOrder } from "./utils/sorts.js";
+import { CONNECT_DB, GET_DB } from "~/config/mongodb";
 
-const app = express();
+const START_SEVER = () => {
+  const app = express();
 
-const hostname = "localhost";
-const port = 2728;
+  const hostname = "localhost";
+  const port = 2728;
 
-app.get("/", (req, res) => {
-  console.log(
-    mapOrder(
-      [
-        { id: "id-1", name: "One" },
-        { id: "id-2", name: "Two" },
-        { id: "id-3", name: "Three" },
-        { id: "id-4", name: "Four" },
-        { id: "id-5", name: "Five" },
-      ],
-      ["id-5", "id-4", "id-2", "id-3", "id-1"],
-      "id"
-    )
-  );
-  res.end("<h1>Hello World!</h1><hr>");
-});
+  app.get("/", async (request, response) => {
+    console.log(await GET_DB().listCollections().toArray());
+    response.end("hello world!");
+  });
 
-app.listen(port, hostname, () => {
-  console.log(`Backend Nodejs is running on port: http://${hostname}:${port}`);
-});
+  app.listen(port, hostname, () => {
+    console.log(`Backend Nodejs is running at: http://${hostname}:${port}`);
+  });
+};
+
+(async () => {
+  try {
+    console.log("Connnecting to MongoDB Cloud Atlas...");
+    await CONNECT_DB();
+    console.log("Connnected to MongoDB Cloud Atlas!");
+
+    START_SEVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
